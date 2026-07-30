4fdf30eeb5de9e27429618882d238fc1df6122c6
Harry <habib.habibb676@gmail.com>
2026-07-30 12:20:28 +1000

    sound  test
    we make the button makes sound and we tested it and it's working!

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.body.style.backgroundColor = "red";
+// document.body.style.backgroundColor = "red";
+// find my test button
+const testButton = document.getElementById("test-button");
+
+//create instrument and connect to audio
+const synth = new TouchEvent.Synth().ToDestination();
+
+// do something when we click thay button
+testbutton.addEventlistener("click", playTestNote);
+
+function playTestNote(){
+  synth.triggerAttackRelease("C4", "8n"); 
+}
